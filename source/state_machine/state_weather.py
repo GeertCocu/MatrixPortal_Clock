957baeb16d04fd196a80c0844a7e925919daca7f
Pineapplez <geertcocu@hotmail.com>
2026-04-07 21:53:36 +0200

    Modified weather page: - Added support for weather Icons - Added support for country Icons - Modified layout to include icons

--- source/state_machine/state_weather.py
+++ source/state_machine/state_weather.py
@@ -2,12 +2,13 @@ from state_machine.state import State
 import os
 import time
 import terminalio
+import displayio
 from adafruit_matrixportal.network import Network
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text.label import Label
 
 DATA_SOURCE_FORMAT = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=" + os.getenv('OPENWEATHER_TOKEN') + "&units=metric"
-UPDATE_INTERVAL = 30
+UPDATE_INTERVAL = 300 # Once every 5 minutes
 
 class StateWeather(State):
     def __init__(self, displayWidth, displayHeight, displayGroup, network, location):
@@ -27,23 +28,31 @@ class StateWeather(State):
 
         self.fail_color = 0xFF0000
         self.succes_color = 0x85FF00
+        self.weather_group = displayio.Group()
 
         self.temp_label = Label(terminalio.FONT)
-        self.temp_label.anchor_point = (0.5, 0.5)
-        self.temp_label.anchored_position = (self.displayWidth / 2, self.displayHeight / 2)
+        self.temp_label.anchor_point = (0, 0.5)
+        self.temp_label.anchored_position = (16, 8)
         self.temp_label.text = "9999 C"
         self.temp_label.color = self.fail_color
+        self.weather_group.append(self.temp_label)
 
+        self.weather_icon_group = displayio.Group()
+        self.weather_group.append(self.weather_icon_group)
+
+        self.location_icon_group = displayio.Group()
+        self.location_icon_group.y = 16
+        self.weather_group.append(self.location_icon_group)
 
     def load(self):
         super().load()
         self.fetch_weather()
-        self.displayGroup.append(self.temp_label)
+        self.displayGroup.append(self.weather_group)
         print("Hello Weather")
 
     def unload(self):
         super().unload()
-        self.displayGroup.remove(self.temp_label)
+        self.displayGroup.remove(self.weather_group) 
         print("Goodbye Weather")
 
     def update(self):
@@ -64,6 +73,28 @@ class StateWeather(State):
             return
         if self.response is not None:
             self.temp_label.color = self.succes_color
-            self.temp_label.text = self.location + "\n%d C"%(self.response['main']['temp'])
+            self.temp_label.text = "%d C"%(self.response['main']['temp'])
+            weather_icon = self.response['weather'][0]['icon']
+            self.set_icon(self.weather_icon_group, "/icons/weather/" + weather_icon + ".bmp")
+            location_icon = self.response['sys']['country']
+            self.set_icon(self.location_icon_group, "/icons/countries/" + location_icon + ".bmp")
         else:
             self.temp_label.color = self.fail_color
+    
+    def set_icon(self, icon_group, filename):
+        """The background image to a bitmap file.
+
+        :param filename: The filename of the chosen icon
+
+        """
+        print("Set icon to ", filename)
+        if icon_group:
+            icon_group.pop()
+
+        if not filename:
+            return  # we're done, no icon desired
+
+        icon = displayio.OnDiskBitmap(filename)
+        weather_icon = displayio.TileGrid(icon, pixel_shader=icon.pixel_shader)
+
+        icon_group.append(weather_icon)
